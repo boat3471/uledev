f98ce2b25b974a5e606c9d3c33f547ab9733f02c
xinwenchao <xinwenchao@xinwenchao.copm>
2016-12-08 10:41:04 +0800

    重构

--- ule-server/hosts.js
+++ ule-server/hosts.js
@@ -9,35 +9,29 @@ module.exports = function(){
 	var exists = fs.existsSync(hostsPath);
 	if(!exists) return console.info('\n  Error. hosts 文件找不到！');
 
-	var fileContent = fs.readFileSync(hostsPath);
-	var hosts = uledevData.hosts || [];
-	var content = '';
-	for(var i = 0; i < hosts.length; i++){
-		var host = hosts[i];
-		if(fileContent.indexOf(host) < 0)
-			content += '127.0.0.1 ' + host + '\r\n';
-		console.info('[ULE] 配置HOST: ', host);
-	}
-	if(!content) return;
-	var list = [fileContent, ''];
-	if(fileContent.length == 0) list = [];
-	list.push('## uledev hosts ##');
-	list.push('##\r\n');
-	list.push(content);
-	list.push('##');
-	list.push('## uledev hosts ##');
-	fs.writeFile(hostsPath, list.join('\r\n'), function(err){
-		if(err){
-			switch(err.errno){
-				case -4048:
-					console.info('Error. 请修改HOSTS文件权限, HOSTS文件为只读权限, 无法写入！');
-					break;
-				default:
-					console.info(err);
-					break;
+	var oldhosts = fs.readFileSync(hostsPath).toString();
+	var newhosts = uledevData.hosts || '127.0.0.1 www.dev.ule.com i0.dev.ulecdn.com i1.dev.ulecdn.com i2.dev.ulecdn.com i3.dev.ulecdn.com';
+	oldhosts = oldhosts.replace(/## uledev hosts ##/g, '').replace(/\n/g, '').replace(/\r/g, '').replace(/127.0.0.1/g, '\r\n127.0.0.1');
+	var list = [oldhosts, ''];
+	if(oldhosts.indexOf('dev.ulecdn.com') > 0 && oldhosts.indexOf('dev.ule.com') > 0) {
+
+	} else {
+		list.push('## uledev hosts ##');
+		list.push(newhosts);
+		list.push('## uledev hosts ##');
+		fs.writeFile(hostsPath, list.join('\r\n'), function(err){
+			if(err){
+				switch(err.errno){
+					case -4048:
+						console.info('Error. 请修改HOSTS文件权限, HOSTS文件为只读权限, 无法写入！');
+						break;
+					default:
+						console.info(err);
+						break;
+				}
+				assert.equal('');
+			}else{
 			}
-			assert.equal('');
-		}else{
-		}
-	});
+		});
+	}
 };
